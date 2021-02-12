e6abbc6bfa6f5da6afff5f3c229e99031e22ac88
Sarangdeep Singh <sarangdeep.singh93@gmail.com>
2021-02-12 04:30:13 -0600

    C - Kefa and Park

---
+++ src/KefaAndPark_580C.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+/**
+ * @author Sarangdeep Singh <sarangdeep.singh@outlook.com>
+ * @link
+ */
+public class KefaAndPark_580C {
+}
